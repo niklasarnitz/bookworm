bf4f278a9c978daf3ee5da12bb35bea35993fe67
Niklas Arnitz <narnitz@churchtools.de>
2025-06-03 22:27:14 +0200

    Revert "maybe fixed build"
    This reverts commit 7754413dac44bdf2663140d2650933e4a0ac4608.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,10 @@
  * for Docker builds.
  */
 import "./src/env.js";
+import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import("next").NextConfig} */
-const config = {
+const config = withSentryConfig({
   reactStrictMode: true,
   experimental: {
     nodeMiddleware: true,
@@ -25,6 +26,24 @@ const config = {
       },
     ],
   },
-};
+}, {
+  org: "bookworm",
+  project: "bookworm-web",
+  sentryUrl: "https://glitchtip.app.niklas.services/",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/api/tunnel",
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+  automaticVercelMonitors: false,
+});
 
 export default config;
--- src/env.js
+++ src/env.js
@@ -21,6 +21,7 @@ export const env = createEnv({
     MINIO_SECRET_KEY: z.string().default("minioadmin"),
     MINIO_BUCKET_NAME: z.string().default("bookworm"),
     MINIO_REGION: z.string().default("us-east-1"),
+    SENTRY_DSN: z.string().nonempty()
   },
 
   /**
@@ -29,6 +30,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
+    NEXT_PUBLIC_SENTRY_DSN: z.string().nonempty(),
   },
 
   /**
@@ -46,6 +48,8 @@ export const env = createEnv({
     MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY,
     MINIO_BUCKET_NAME: process.env.MINIO_BUCKET_NAME,
     MINIO_REGION: process.env.MINIO_REGION,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially

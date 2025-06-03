33f402d1ed4314bf8a60ac13a1f907950f5dcaef
Niklas Arnitz <narnitz@churchtools.de>
2025-06-03 18:56:25 +0200

    added sentry monitoring to glitchtip

--- next.config.js
+++ next.config.js
@@ -3,28 +3,47 @@
  * for Docker builds.
  */
 import "./src/env.js";
+import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import("next").NextConfig} */
-const config = {
-    reactStrictMode: true,
-    experimental: {
-        nodeMiddleware: true,
-        reactCompiler: true
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: process.env.MINIO_ENDPOINT || 'localhost',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: process.env.MINIO_ENDPOINT || 'localhost',
-                pathname: '/**',
-            },
-        ],
-    },
-};
+const config = withSentryConfig({
+  reactStrictMode: true,
+  experimental: {
+    nodeMiddleware: true,
+    reactCompiler: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: process.env.MINIO_ENDPOINT || "localhost",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: process.env.MINIO_ENDPOINT || "localhost",
+        pathname: "/**",
+      },
+    ],
+  },
+}, {
+  org: "bookworm",
+  project: "bookworm-web",
+  sentryUrl: "https://glitchtip.app.niklas.services/",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: "/api/tunnel",
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+  automaticVercelMonitors: false,
+});
 
 export default config;
